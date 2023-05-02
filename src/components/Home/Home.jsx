f4e96597fdd013d10a163d627a18b19075dfaf4c
IconicLuck <uhasib959@gmail.com>
2023-05-02 19:52:55 +0600

    added header part of home route

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import bg from '../../assets/images/yeh-xintong-5wwv5PgSnLI-unsplash.jpg'
 
 const Home = () => {
     return (
         <div>
-            <h1>This is Home component</h1>
+            <div className='h-[800px]' style={{background: `linear-gradient(rgba(0, 0, 0, 0.60), rgba(0, 0, 0, 0.60)), url(${bg})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundPosition: 'center'}}>
+                <div className='h-full flex flex-col justify-center items-center'>
+                    <h1 className='text-4xl font-bold text-white text-center'>Food & <span className='text-green-500'>Taste</span></h1>
+                    <p className='text-white font-semibold'>A recipe is a formula of ingredients and a list of instructions for creating prepared foods. It is used to control quality, quantity, and food costs in a food service operation.</p>
+                </div>
+            </div>
         </div>
     );
 };
