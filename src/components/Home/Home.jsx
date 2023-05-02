8312d88ae1f57d404832044afc353dabd4d5a497
IconicLuck <uhasib959@gmail.com>
2023-05-02 20:04:31 +0600

    redesigned header of the home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import bg from '../../assets/images/yeh-xintong-5wwv5PgSnLI-unsplash.jpg'
+import bg from '../../assets/images/pngegg.png'
 
 const Home = () => {
     return (
         <div>
-            <div className='h-[800px]' style={{background: `linear-gradient(rgba(0, 0, 0, 0.60), rgba(0, 0, 0, 0.60)), url(${bg})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundPosition: 'center'}}>
-                <div className='h-full flex flex-col justify-center items-center'>
-                    <h1 className='text-4xl font-bold text-white text-center'>Food & <span className='text-green-500'>Taste</span></h1>
-                    <p className='text-white font-semibold'>A recipe is a formula of ingredients and a list of instructions for creating prepared foods. It is used to control quality, quantity, and food costs in a food service operation.</p>
+            <div className='flex justify-center items-center px-40 bg-green-100'>
+                <div className='w-1/2'>
+                    <h1 className='text-5xl mb-2 font-bold text-black text-center'>Food & <span className='text-green-500'>Taste</span></h1>
+                    <p className='text-black text-xl font-semibold'>A recipe is a formula of ingredients and a list of instructions for creating prepared foods. It is used to control quality, quantity, and food costs in a food service operation.</p>
+                </div>
+                <div className='w-1/2'>
+                    <img className='w-full h-fit' src={bg} alt="" />
                 </div>
             </div>
         </div>
