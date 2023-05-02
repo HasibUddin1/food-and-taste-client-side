644758238a5f8107afc5779930bc92a8ba4a4ed0
IconicLuck <uhasib959@gmail.com>
2023-05-02 12:04:51 +0600

    implemented create user with email and password

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -1,5 +1,5 @@
 import React, { createContext } from 'react';
-import { GithubAuthProvider, GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import { GithubAuthProvider, GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, signInWithPopup } from "firebase/auth";
 import app from '../firebase/firebase.config';
 
 export const AuthContext = createContext(null)
@@ -10,7 +10,7 @@ const googleProvider = new GoogleAuthProvider()
 
 const gitHubProvider = new GithubAuthProvider()
 
-const AuthProvider = ({children}) => {
+const AuthProvider = ({ children }) => {
 
     const googleSignIn = () => {
         return signInWithPopup(auth, googleProvider)
@@ -20,9 +20,14 @@ const AuthProvider = ({children}) => {
         return signInWithPopup(auth, gitHubProvider)
     }
 
+    const createUser = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
     const authInfo = {
         googleSignIn,
-        gitHubSignIn
+        gitHubSignIn,
+        createUser
     }
 
     return (
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,17 +1,59 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../Providers/AuthProvider';
 
 
 const Register = () => {
 
+    const [error, setError] = useState('')
+    const [success, setSuccess] = useState('')
+
+    const { createUser } = useContext(AuthContext)
+
+    const handleRegister = event => {
+        event.preventDefault()
+
+        const form = event.target
+        const name = form.name.value
+        const photo = form.photo.value
+        const email = form.email.value
+        const password = form.password.value
+        const confirmPassword = form.confirm.value
+        setSuccess('')
+        setError('')
+
+        if (password !== confirmPassword) {
+            setError('Your password did not match')
+            return
+        }
+
+        if ((email, password)) {
+            createUser(email, password)
+                .then(result => {
+                    const registeredUser = result.user
+                    console.log(registeredUser)
+                    setSuccess('User has been successfully created')
+                    setError('')
+                })
+                .catch(error => {
+                    console.error(error)
+                    setError(error.message)
+                })
+        }
+        else{
+            setError('Email and Password fields cannot be empty')
+        }
+    }
+
     return (
         <div className='w-9/12 mx-auto mt-10'>
-            <form>
+            <form onSubmit={handleRegister}>
                 <div className='flex flex-col gap-5 w-1/2 mx-auto'>
                     <input className='border-2 rounded-lg px-3 py-1' type="text" name="name" id="name" placeholder='Your Name' required />
                     <input className='border-2 rounded-lg px-3 py-1' type="text" name='photo' id='photo' placeholder='Your Photo' />
                     <input className='border-2 rounded-lg px-3 py-1' type="email" name="email" id="email" placeholder='Your Email' required />
                     <input className='border-2 rounded-lg px-3 py-1' type="password" name="password" id="password" placeholder='Your Password' required />
+                    <input className='border-2 rounded-lg px-3 py-1' type="password" name="confirm" id="confirm" placeholder='Confirm Your Password' required />
                 </div>
                 <div className='w-1/4 mx-auto text-center mt-5'>
                     <input className='bg-green-500 hover:bg-green-700 cursor-pointer ease-in-out duration-200 px-6 py-2 rounded-lg w-1/2 text-white font-semibold' type="submit" value="Register" />
@@ -20,6 +62,14 @@ const Register = () => {
                     <p className='text-center font-semibold'>Already have an account? <Link className='text-purple-500 hover:text-purple-700' to='/login'>Please Login</Link></p>
                 </div>
             </form>
+            {
+                error &&
+                <h4 className='text-red-500 font-semibold text-xl text-center mt-2'>Error: {error}</h4>
+            }
+            {
+                success &&
+                <h4 className='text-green-500 font-semibold text-xl mt-2'>{success}</h4>
+            }
         </div>
     );
 };
