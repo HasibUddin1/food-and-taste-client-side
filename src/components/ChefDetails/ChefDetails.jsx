1595243474b2c2071dcb5fc58dcc2c364e37f3a1
IconicLuck <uhasib959@gmail.com>
2023-05-02 22:54:13 +0600

    added recipe cards for each recipes on each chef details

--- src/components/ChefDetails/ChefDetails.jsx
+++ src/components/ChefDetails/ChefDetails.jsx
@@ -1,26 +1,38 @@
 import React from 'react';
 import { Link, useLoaderData } from 'react-router-dom';
 import { FaThumbsUp } from "react-icons/fa";
+import RecipeCard from '../RecipeCard/RecipeCard';
 
 const ChefDetails = () => {
 
     const chefDetail = useLoaderData()
-    console.log(chefDetail)
-    const { name, yearsOfExperience, bio, likes, imageUrl } = chefDetail
+    // console.log(chefDetail)
+    const { name, yearsOfExperience, bio, likes, imageUrl, recipes } = chefDetail
 
     return (
-        <div className='flex gap-5 justify-center items-center mt-10 mb-10 px-40'>
-            <div className='flex flex-col gap-5'>
-                <h1 className='text-6xl font-bold'>{name}</h1>
-                <h3 className='text-3xl font-semibold'>Years of Experience: {yearsOfExperience}</h3>
-                <p className='text-xl'>{bio}</p>
-                <h4 className='text-2xl flex items-center gap-2'>Likes: {likes} <FaThumbsUp></FaThumbsUp></h4>
-                <Link to='/'>
-                    <button className='bg-green-500 hover:bg-green-700 ease-in-out duration-200 px-6 py-2 text-white rounded-xl w-1/6 font-semibold'>Go Back</button>
-                </Link>
+        <div>
+            <div className='flex gap-5 justify-center items-center mt-10 mb-10 px-40'>
+                <div className='flex flex-col gap-5'>
+                    <h1 className='text-6xl font-bold'>{name}</h1>
+                    <h3 className='text-3xl font-semibold'>Years of Experience: {yearsOfExperience}</h3>
+                    <p className='text-xl'>{bio}</p>
+                    <h4 className='text-2xl flex items-center gap-2'>Likes: {likes} <FaThumbsUp></FaThumbsUp></h4>
+                    <Link to='/'>
+                        <button className='bg-green-500 hover:bg-green-700 ease-in-out duration-200 px-6 py-2 text-white rounded-xl w-1/6 font-semibold'>Go Back</button>
+                    </Link>
+                </div>
+                <div>
+                    <img className='rounded-xl' src={imageUrl} alt="" />
+                </div>
             </div>
-            <div>
-                <img className='rounded-xl' src={imageUrl} alt="" />
+            <h2 className='text-4xl text-center font-bold mb-10'>Find Out the Best Recipes of {name}</h2>
+            <div className='w-9/12 mx-auto grid grid-cols-3 gap-5 mb-10'>
+                {
+                    recipes.map(recipe => <RecipeCard
+                        key={recipe.id}
+                        recipe={recipe}
+                    ></RecipeCard>)
+                }
             </div>
         </div>
     );
---
+++ src/components/RecipeCard/RecipeCard.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const RecipeCard = ({ recipe }) => {
+    // console.log(recipe)
+    const { name, description, ingredients} = recipe
+    return (
+        <div>
+            <div className="card w-96 h-96 border-2 rounded-xl">
+                <div className="card-body">
+                    <h2 className="card-title">{name}</h2>
+                    <p>{description}</p>
+                    {
+                        ingredients.map(ingredient => <li
+                            key={ingredient.id}
+                        >{ingredient.name}</li>)
+                    }
+                    <div className="card-actions justify-end">
+                        <button className="bg-green-500 hover:bg-green-700 ease-in-out duration-200 px-6 py-2 text-white rounded-xl font-semibold">Add to Favorites</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default RecipeCard;
