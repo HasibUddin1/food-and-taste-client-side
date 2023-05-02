8342ab39aec6f70bd46ec3db92b1abf14912be77
IconicLuck <uhasib959@gmail.com>
2023-05-02 10:21:06 +0600

    added header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Header.css'
 
 const Header = () => {
     return (
-        <div>
-            <Link to='/'>Home</Link>
-            <Link to='/blog'>Blog</Link>
-            <Link to='/login'>Login</Link>
-            <Link to='/register'>Register</Link>
+        <div className='bg-green-500 flex justify-between items-center font-semibold px-10 py-3'>
+            <div>
+                <h1 className='text-3xl'>Food and Taste</h1>
+            </div>
+            <div className='flex gap-5 anchors-container'>
+                <Link to='/'>Home</Link>
+                <Link to='/blog'>Blog</Link>
+                <Link to='/login'>Login</Link>
+                <Link to='/register'>Register</Link>
+            </div>
         </div>
     );
 };
