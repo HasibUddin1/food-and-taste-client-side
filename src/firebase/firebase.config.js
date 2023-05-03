51d965c8bb62750bab45a0a7daa2f6f5b7ec3ca6
IconicLuck <uhasib959@gmail.com>
2023-05-03 06:21:20 +0600

    added environment variables to hide api keys

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC2v3u89AqxZh9j8BjDdsq8Xa01IOBI7zM",
-  authDomain: "chef-recipe-hunter-397b0.firebaseapp.com",
-  projectId: "chef-recipe-hunter-397b0",
-  storageBucket: "chef-recipe-hunter-397b0.appspot.com",
-  messagingSenderId: "919403164680",
-  appId: "1:919403164680:web:ee031b28c77d15cc144dbd"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId
 };
 
 // Initialize Firebase
